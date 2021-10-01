3ca444acc7674b8ea9e5cfbc10cb427459da7f8f
Eric Hazen <hazen@bu.edu>
2021-10-01 12:57:34 -0400

    maybe it's working

--- trenz_pinout/csv2xdc.pl
+++ trenz_pinout/csv2xdc.pl
@@ -1,51 +1,57 @@
 #!/usr/bin/perl
 #
-# convert Trenz text pinout
+# convert Trenz text pinout to Vivado constraint file
 #
 # input:  CSV (spreadsheet) with columns
 #   PACKAGE_PIN - FPGA pin number (e.g. "P12" or "144")
 #   PORT        - HDL port name "sclk" or "b(3)"
-#   IOSTANDARD  - e.g. "LVCMOS18" (optional)
+#   IOSTANDARD  - e.g. "LVCMOS18"
 #
 #   (other columns are ignored but added as comments)
 #
 use Text::CSV qw( csv );
 use Data::Dumper;
 
-my $narg = $#ARGV+1;
+die "Usage:  $0 <csv_file>\n" if( $#ARGV != 0);
 
-if( $#ARGV != 0) {
-    print "Usage:  $0 <csv_file>\n";
-    exit;
-}
-
+# read entire CSV file and return an array of hashes
 my $aoh = csv ( in => $ARGV[0], headers => "auto");
 
-my $header = $aoh->[0];
-my @cols = keys %{$header};
+# get the list of columns from the first row
+my $header = $aoh->[0];		# first row as a hash
+my @cols = keys %{$header};	# extract the keys (column names)
 
+# be sure required columns exist
 if( !$header->{"PACKAGE_PIN"} || !$header->{"PORT"}) {
     print "Missing PACKAGE_PIN or PORT colum\n";
     exit;
 }
 
+# array to store "extra" column names
 my @extras;
 
-foreach my $col ( @{$cols}) {
-    push @extras, $col
+# make a list of "extra" column names
+foreach my $col ( @cols) {
+    push @extras, $col 
 	if( $col ne "PACKAGE_PIN" && $col ne "PORT" && $col ne "IOSTANDARD");
 }
 
 
+# loop over each row in the file
 foreach my $row ( @{$aoh}) {
-    my $port = " [get_ports " .	$row->{"PORT"} . "]";
-    print "set_property PACKAGE_PIN ", $row->{"PACKAGE_PIN"}, $port, "\n";
-    print "IOSTANDARD ", $row->{"IOSTANDARD"}, $port, "\n"
-	if( $row->{"IOSTANDARD"});
-    print "#";
+    # print a comment with the extra columns
+    print "\n# ";
     foreach $extra ( @extras ) {
-	print $extra, "=", $row->{$extra}, " ";
-    }
+	print $extra, "=", $row->{$extra}," ";
+    };
     print "\n";
+
+    # create the [get_ports xxx] clause
+    my $port = " [get_ports " .	$row->{"PORT"} . "]";
+    # emit the PACKAGE_PIN statement
+    print "set_property PACKAGE_PIN ", $row->{"PACKAGE_PIN"}, $port, "\n";
+    # emit the optional IOSTANDARD statement
+    print "set_property IOSTANDARD ", $row->{"IOSTANDARD"}, $port, "\n"
+	if( $row->{"IOSTANDARD"});
 }
 
